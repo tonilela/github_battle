a26936624471fd58fe9e956eb54dd3c82c3981fa
tonilela <tonilela@gmail.com>
2018-10-22 17:42:30 +0200

    webpack error fixed, renaming webpack config file

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-require('index.css')
+import './index.css'
 
 class App extends React.Component {
     render(){
         return(
             <div>
-                Helloooo
+                da da
             </div>    
         )
     }
